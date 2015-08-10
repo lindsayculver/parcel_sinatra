1555e809a8178ea2ea17dd2a54c222439b58c727
lindsay culver & chris underwood <student@epicodus.com>
2015-08-10 14:05:48 -0700

    add the first spec

---
+++ spec/parcel_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('parcel')
+
+describe(Parcel) do
+
+  describe("#volume") do
+    it("returns the volume based on length, height and width")
+    test_volume = Parcel.new(5, 4, 3)
+    expect(test_volume.volume()).to(eq(60))
+  end
+end
