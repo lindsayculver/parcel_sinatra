5063ada3bcff5683d2ad156fd9a28f41562ae260
lindsay culver & chris underwood <student@epicodus.com>
2015-08-10 15:04:20 -0700

    return the total cost based on volume, speed and weight

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,13 +1,35 @@
 class Parcel
-  define_method(:initialize) do |length, height, width|
+  define_method(:initialize) do |length, height, width, speed, weight|
     @length = length
     @height = height
     @width = width
+    @speed = speed
+    @weight = weight
+    @package_volume
+
   end
 
   define_method(:volume) do
-    package_volume = @length * @height * @width
+    @package_volume = @length * @height * @width
     #@length.*(@height.*(@width))
+  end
+
+  define_method(:cost_to_ship) do
+
+      volume_cost = volume() * 2
+
+        if @speed = 1
+        speed_cost = 5
+      elsif @speed = 2
+        speed_cost = 2
+      else
+        speed_cost = 0
+      end
+
+      cost_weight = @weight * 2
+
+      total_cost = cost_weight + volume_cost + speed_cost
+
   end
 
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -5,15 +5,15 @@ describe(Parcel) do
 
   describe("#volume") do
     it("returns the volume based on length, height and width") do
-      test_volume = Parcel.new(5, 4, 3)
+      test_volume = Parcel.new(5, 4, 3, 0, 0)
       expect(test_volume.volume()).to(eq(60))
     end
   end
 
   describe("#cost_to_ship") do
-    it("retuns the cost based on volume, speed and weight") do
-      test_cost_to_ship = Parcel.new(60, 2, 10)
-      expect(test_cost_to_ship.cost_to_ship()).to(eq(150))
+    it("returns the cost based on volume, speed and weight") do
+      test_cost_to_ship = Parcel.new(5, 4, 3, 2, 10)
+      expect(test_cost_to_ship.cost_to_ship()).to(eq(145))
     end
   end
 end
