5adebe2130690cba661f6900eb4f0b5a063d8785
lindsay culver & chris underwood <student@epicodus.com>
2015-08-10 14:25:52 -0700

    add the second spec for calculating cost

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -5,8 +5,15 @@ describe(Parcel) do
 
   describe("#volume") do
     it("returns the volume based on length, height and width") do
-    test_volume = Parcel.new(5, 4, 3)
-    expect(test_volume.volume()).to(eq(60))
+      test_volume = Parcel.new(5, 4, 3)
+      expect(test_volume.volume()).to(eq(60))
+    end
   end
+
+  describe("#cost_to_ship") do
+    it("retuns the cost based on volume, speed and weight") do
+      test_cost_to_ship = Parcel.new(60, 2, 10)
+      expect(test_cost_to_ship.cost_to_ship()).to(eq(150))
+    end
   end
 end
